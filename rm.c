e13f874f0be1b414d66bc7ed81a6319e433ec70e
viz <vizs@github.com>
2019-06-23 20:41:29 +0530

    rm: add verbose flag

--- rm.c
+++ rm.c
@@ -5,36 +5,43 @@
 static void
 usage(void)
 {
-	eprintf("usage: %s [-f] [-Rr] file ...\n", argv0);
+    eprintf("usage: %s [-f] [-i] [-Rr] file ...\n", argv0);
 }
 
 int
 main(int argc, char *argv[])
 {
-	struct recursor r = { .fn = rm, .hist = NULL, .depth = 0, .maxdepth = 1,
-	                      .follow = 'P', .flags = 0 };
+    struct recursor r = { .fn = rm, .hist = NULL, .depth = 0, .maxdepth = 1,
+                          .follow = 'P', .flags = 0 };
+    int verbose = 0;
 
-	ARGBEGIN {
-	case 'f':
-		r.flags |= SILENT;
-		break;
-	case 'R':
-	case 'r':
-		r.maxdepth = 0;
-		break;
-	default:
-		usage();
-	} ARGEND
+    ARGBEGIN {
+    case 'f':
+        r.flags |= SILENT;
+        break;
+    case 'R':
+    case 'r':
+        r.maxdepth = 0;
+        break;
+    case 'v':
+        verbose = 1;
+        break;
+    default:
+        usage();
+    } ARGEND
 
-	if (!argc) {
-		if (!(r.flags & SILENT))
-			usage();
-		else
-			return 0;
-	}
+    if (!argc) {
+        if (!(r.flags & SILENT))
+            usage();
+        else
+            return 0;
+    }
 
-	for (; *argv; argc--, argv++)
-		recurse(*argv, NULL, &r);
+    for (; *argv; argc--, argv++) {
+        if (verbose)
+            fprintf(stderr, "removing %s", *argv);
+        recurse(*argv, NULL, &r);
+    }
 
-	return rm_status || recurse_status;
+    return rm_status || recurse_status;
 }
